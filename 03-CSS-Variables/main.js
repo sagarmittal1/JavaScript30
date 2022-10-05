495623d6f9ec9f9dd87e3b85c0140a6ac234426e
Sagar Mittal <mittalsagar006@gmail.com>
2022-10-06 01:28:20 +0530

    made some changes & fix typo

--- 03-CSS-Variables/main.js
+++ 03-CSS-Variables/main.js
@@ -4,6 +4,13 @@ function handleUpdate() {
     // ! this.dataset give all the data-* in that html tag.
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+    // this thing also works
+    // if(this.name !== 'base') {
+    //     document.documentElement.style.setProperty(`--${this.name}`, `${this.value}px`)
+    // }else{
+    //     document.documentElement.style.setProperty(`--${this.name}`, `${this.value}`)
+    // }
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
