66e177c5081c979ea6b733f32b06503f34d23ba5
Sagar Mittal <mittalsagar006@gmail.com>
2022-12-10 23:43:23 +0530

    started making localStorage project

---
+++ 14-LocalStorage/main.js
@@ -0,0 +1,27 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
+
+function addItem (e) {
+    e.preventDefault();
+    const text = (this.querySelector('[name=item]')).value;
+    const item = {
+        text,
+        done: false
+    }
+    items.push(item);
+    populateList(items, itemsList);
+    this.reset();
+}
+
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+            <li>
+                <label>${plate.text}</label>
+            </li>
+        `
+    }).join('');
+}
+
+addItems.addEventListener('submit', addItem);
